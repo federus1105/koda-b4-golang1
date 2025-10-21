527b2614e2a8bc533170bc6af06210fb383e5ffd
federus1105 <federusrudi@gmail.com>
2025-10-21 14:55:49 +0700

    Feat (main) : function feature to calculate area and circumference using scan

--- main.go
+++ main.go
@@ -9,15 +9,21 @@ func CalculateArea(r float32) float32 {
 	return math.Pi * r * r
 }
 
-func Circumference (r float32) float32 {
-	return  2 * math.Pi * r
+func Circumference(r float32) float32 {
+	return 2 * math.Pi * r
 }
 func main() {
 	// ========= Hitung Luas
-	luas := CalculateArea(10.0)
+	var inputwide float32
+	fmt.Println("Masukkan angka")
+	fmt.Scanln(&inputwide)
+	luas := CalculateArea(inputwide)
 	fmt.Printf("Luas lingkaran %2.f\n", luas)
 
 	// ======== Hitung Keliling
-	keliling := Circumference(10.0)
+	var inputaround float32
+	fmt.Println("Masukan angka")
+	fmt.Scanln(&inputaround)
+	keliling := Circumference(inputaround)
 	fmt.Printf("Keliling Lingkaran %2.f\n", keliling)
 }
