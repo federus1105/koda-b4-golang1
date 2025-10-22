c5c1e671a509973b3b431c0d1295765e8a6832f1
federus1105 <federusrudi@gmail.com>
2025-10-22 13:36:39 +0700

    Feat ( main ) : function method getwide and getaround

--- main.go
+++ main.go
@@ -5,25 +5,41 @@ import (
 	"math"
 )
 
-func CalculateArea(r float32) float32 {
-	return math.Pi * r * r
+type Circle struct {
+	Radius float32
 }
 
-func Circumference(r float32) float32 {
-	return 2 * math.Pi * r
+func (c Circle) getWide() float32 {
+	return math.Pi * c.Radius * c.Radius
 }
+
+func (c Circle) getAround() float32 {
+	return 2 * math.Pi * c.Radius
+}
+
 func main() {
-	// ========= Hitung Luas
-	var inputwide float32
+	var input float32
 	fmt.Println("Masukkan angka")
-	fmt.Scanln(&inputwide)
-	luas := CalculateArea(inputwide)
-	fmt.Printf("Luas lingkaran %2.f\n", luas)
+	fmt.Scanln(&input)
+	result := Circle{input}
+
+	wide := result.getWide()
+	around := result.getAround()
+
+	fmt.Println("Keliling nya adalah", around)
+	fmt.Println("Luas nya adalah", wide)
+
+	// ========= Hitung Luas
+
+	// fmt.Println("Masukkan angka")
+	// fmt.Scanln(&inputwide)
+	// luas := CalculateArea(inputwide)
+	// fmt.Printf("Luas lingkaran %2.f\n", luas)
 
 	// ======== Hitung Keliling
-	var inputaround float32
-	fmt.Println("Masukan angka")
-	fmt.Scanln(&inputaround)
-	keliling := Circumference(inputaround)
-	fmt.Printf("Keliling Lingkaran %2.f\n", keliling)
+	// var inputaround float32
+	// fmt.Println("Masukan angka")
+	// fmt.Scanln(&inputaround)
+	// keliling := Circumference(inputaround)
+	// fmt.Printf("Keliling Lingkaran %2.f\n", keliling)
 }
